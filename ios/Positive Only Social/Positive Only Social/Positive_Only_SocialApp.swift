0040ea3b6bc2cb20b23cb71638cb6d1dc8eb88da
andrewkatson <katsonandrew3.5@gmail.com>
2025-09-02 12:18:02 -0700

    Basic files for ios, android and backend

---
+++ ios/Positive Only Social/Positive Only Social/Positive_Only_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Positive_Only_SocialApp.swift
+//  Positive Only Social
+//
+//  Created by Andrew Katson on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Positive_Only_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
