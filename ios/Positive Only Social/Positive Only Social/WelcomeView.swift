733b8ab630e1a20c36594294c3f8d3ef3d78c8ad
Andrew Katson <andrewkatson@andrewsacstudio.lan>
2025-10-31 16:08:14 -0700

    remove extra letter

--- ios/Positive Only Social/Positive Only Social/WelcomeView.swift
+++ ios/Positive Only Social/Positive Only Social/WelcomeView.swift
@@ -23,7 +23,7 @@ struct WelcomeView: View {
     @State private var path = NavigationPath()
     
     // Unique identifiers for Keychain items
-    private let keychainService = "cpositive-only-social.Positive-Only-Social"
+    private let keychainService = "positive-only-social.Positive-Only-Social"
     private let sessionAccount = "userSessionToken"
     private let rememberMeAccount = "userRememberMeTokens"
 
