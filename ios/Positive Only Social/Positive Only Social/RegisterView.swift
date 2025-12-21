54b656d3138196a7af6c7965e5e356f4b68fa7f3
Andrew Katson <andrewkatson@Andrews-Mac-Studio.local>
2025-12-21 09:14:24 -0800

    Fix the build error

--- ios/Positive Only Social/Positive Only Social/RegisterView.swift
+++ ios/Positive Only Social/Positive Only Social/RegisterView.swift
@@ -137,7 +137,6 @@ struct RegisterView: View {
             Text("We collect your username and password for authentication. We do not store your date of birth or any other personal information. We store your posts, comments, and related metadata such as like counts and reports. We also track follower/following relationships and blocked users to maintain the social environment.")
         }
     }
-    }
 
     // MARK: - Registration Action
     private func register() {
