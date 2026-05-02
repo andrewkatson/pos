3a7d429fe4542d4070a28001d4396fe2d03c951c
Andrew Katson <katsonandrew3.5@gmail.com>
2026-05-01 20:12:17 -0700

    Update ssl redirect
    The alb health checker uses http apparently

--- backend/pos_backend/settings.py
+++ backend/pos_backend/settings.py
@@ -37,7 +37,8 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 # Security settings for production
-SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "True").lower() == "true"
+SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "False").lower() == "true"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
