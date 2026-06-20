cf33f11ecc9f7725440fe6d65c16b602aee7f3ae
Nithesh R <mudsunithesh0812@gmail.com>
2026-06-20 01:00:24 -0500

    Refactored code

--- backend/user_system/tests/test_new_device_email.py
+++ backend/user_system/tests/test_new_device_email.py
@@ -1,6 +1,5 @@
 from django.core import mail
 from django.urls import reverse
-
 from .test_constants import false, true
 from .test_parent_case import PositiveOnlySocialTestCase
 from ..models import KnownDevice, PositiveOnlySocialUser
@@ -19,7 +18,8 @@ class NewDeviceEmailTests(PositiveOnlySocialTestCase):
         super().setUp()
         super().register_user_and_setup_local_fields()
         self.login_url = reverse('login_user')
-        
+        self.registration_email= list(mail.outbox)
+        mail.outbox.clear()
     def _login(self, remote_addr, remember_me=false):
         data = {
             'username_or_email': self.local_username,
@@ -37,14 +37,13 @@ class NewDeviceEmailTests(PositiveOnlySocialTestCase):
     def test_registration_records_device_without_emailing(self):
         user = PositiveOnlySocialUser.objects.get(username=self.local_username)
         self.assertTrue(KnownDevice.objects.filter(user=user, ip=REGISTRATION_IP).exists())
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject,"Welcome to Good Vibes Only")
+        self.assertEqual(len(self.registration_email), 1)
+        self.assertEqual(self.registration_email[0].subject,"Welcome to Good Vibes Only")
 
 
     # --- login from a new device ---
 
     def test_login_from_new_ip_sends_email_and_records_device(self):
-        mail.outbox.clear()
         self._login(NEW_IP)
 
         user = PositiveOnlySocialUser.objects.get(username=self.local_username)
@@ -55,13 +54,11 @@ class NewDeviceEmailTests(PositiveOnlySocialTestCase):
         self.assertIn(NEW_IP, message.body)
 
     def test_login_from_known_ip_does_not_send_email(self):
-        mail.outbox.clear()
         # Same IP that registration recorded -> not a new device.
         self._login(REGISTRATION_IP)
         self.assertEqual(len(mail.outbox), 0)
 
     def test_repeated_login_from_same_new_ip_emails_only_once(self):
-        mail.outbox.clear()
         self._login(NEW_IP)
         self._login(NEW_IP)
         self.assertEqual(len(mail.outbox), 1)
@@ -69,7 +66,6 @@ class NewDeviceEmailTests(PositiveOnlySocialTestCase):
             KnownDevice.objects.filter(user__username=self.local_username, ip=NEW_IP).count(), 1)
 
     def test_each_distinct_new_ip_emails(self):
-        mail.outbox.clear()
         self._login(NEW_IP)
         self._login(ANOTHER_NEW_IP)
         self.assertEqual(len(mail.outbox), 2)
@@ -77,7 +73,6 @@ class NewDeviceEmailTests(PositiveOnlySocialTestCase):
     # --- remember-me login ---
 
     def test_remember_me_login_from_new_ip_sends_email(self):
-        mail.outbox.clear()
         # Establish a remember-me cookie from the (already known) registration IP.
         login_data = self._login(REGISTRATION_IP, remember_me=true)
         self.assertEqual(len(mail.outbox), 0)
